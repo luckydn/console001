b9e78d108403b807a9f6a7260072ae4f2d28f1da
luckydn <sp.donbass@gmail.com>
2022-06-28 15:23:07 +0300

    МАША

--- exsample005_name/Program.cs
+++ exsample005_name/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
