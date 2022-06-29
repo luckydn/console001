1762c0f2851d3b575ced0deb76e6c8d2073140c8
luckydn <sp.donbass@gmail.com>
2022-06-30 00:36:20 +0300

    лекция

---
+++ exsample008/Program.cs
@@ -0,0 +1,22 @@
+﻿int max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2> result) result = arg2;
+    if (arg3> result) result = arg3;
+    return result;
+}
+
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 1552;
+int b2 = 23;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23;
+int c3 = 333;
+
+int max1 = Max(a1, b1, c1);
+
+
+Console.WriteLine(max1);
